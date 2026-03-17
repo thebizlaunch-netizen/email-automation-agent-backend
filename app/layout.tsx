33d5a02b5679f90aa1a7386ac5abe9794ca23113
thebizlaunch-netizen <thebizlaunch@gmail.com>
2026-03-17 11:54:18 +0000

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,4 @@
+export const metadata = { title: 'Email Automation Agent API' };
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <html><body>{children}</body></html>;
+}
